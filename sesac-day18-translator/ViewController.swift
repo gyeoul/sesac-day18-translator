fb386d7c3434810408f56f96c134f3f663b3aa61
Gyeoul <clsmay.1st@gmail.com>
2023-08-10 14:49:08 +0900

    Initial Commit

---
+++ sesac-day18-translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesac-day18-translator
+//
+//  Created by 박창현 on 2023.08.10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
